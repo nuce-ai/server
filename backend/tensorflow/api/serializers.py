65d2b7366b5a0fa944c0e355b4660c36cc8b1c06
scriptkid23 <sieunhannhenkutesn@gmail.com>
2019-10-27 00:01:37 +0700

    update
    from django to nodejs

--- backend/tensorflow/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from ..model.models import Image
-from rest_framework import serializers
-
-class ImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Image
-        fields = ('iid','content','label')
--- backend/tensorflow/model/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Image(models.Model):
-    iid = models.CharField(max_length=255)
-    content = models.TextField()
-    label  = models.TextField()
-    def __str__(self):
-        return self.iid
