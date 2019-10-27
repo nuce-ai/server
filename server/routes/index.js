01a53fb44f63dc443a19e68122179a5a119a0a89
scriptkid23 <sieunhannhenkutesn@gmail.com>
2019-10-27 22:23:29 +0700

    update server

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
-
+var Python = require('python-shell');
+const fs = require('fs'); 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+const inputFilePath = 'E:/nuce-ai/server/tensorflow/test.json'
+
+router.post('/',(req,res,next)=>{
+
+    const pathApp = 'E:/nuce-ai/Tensorflow/models/research/object_detection/lab.py'
+    Python.PythonShell.run(pathApp,null,(err,results)=>{
+      if(err) res.sendStatus(404);
+      if(typeof(results) === 'object') res.send('success');
+     })
+     
+  })
+
+  
+
+router.get('/get/object',(req,res,next)=>{
+   if(fs.existsSync(inputFilePath)){
+
+   }
+    fs.readFile(inputFilePath,(err,result)=>{
+      if(err) console.log(err);
+      if(typeof(result) === 'undefined'){
+        res.send("Not data")
+      }
+      else{
+        res.send(JSON.parse(result))
+      }
+    
+    })
+})
 
 module.exports = router;
